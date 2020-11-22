c73f5429cc496f08cacdff7dd3710c6c22d85b3d
changBin-x <bin.chang0@gmail.com>
2020-11-22 13:39:20 +0800

    PCL滤波

---
+++ src/my_pcl_tutorial/src/filtering/passThroughFilter.cc
@@ -0,0 +1,17 @@
+/*
+ * @Date: 2020-11-22 13:17:32
+ * @LastEditTime: 2020-11-22 13:30:36
+ * @Author:  Chang_Bin
+ * @LastEditors: Chang_Bin
+ * @Email: bin_chang@qq.com
+ * @Description:直通滤波
+ */
+
+#include <pcl/filters/passthrough.h>
+#include <pcl/point_types.h>
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+}
