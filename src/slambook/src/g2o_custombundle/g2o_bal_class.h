2a32491c6de98e8c6825b8c51cf16ae9ea7e0d34
changBin-x <bin.chang0@gmail.com>
2021-04-27 14:52:29 +0800

    视觉slam十四讲学习

---
+++ src/slambook/src/g2o_custombundle/g2o_bal_class.h
@@ -0,0 +1,8 @@
+/*
+ * @Date: 2021-04-17 16:59:19
+ * @LastEditTime: 2021-04-17 16:59:20
+ * @Author:  Chang_Bin
+ * @LastEditors: Chang_Bin
+ * @Email: bin_chang@qq.com
+ * @Description:
+ */
---
+++ src/slambook/src/g2o_custombundle/g2o_bundle.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Date: 2021-04-17 16:59:50
+ * @LastEditTime: 2021-04-17 17:15:53
+ * @Author:  Chang_Bin
+ * @LastEditors: Chang_Bin
+ * @Email: bin_chang@qq.com
+ * @Description:
+ */
+#include "common/tools/random.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << RandNormal() << endl;
+
+  return 0;
+}
